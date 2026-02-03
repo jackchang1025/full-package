6fab9d2fd6444ffaa38b4ced0da83977eb11c409
weijiajia <123456@qq.com>
2026-02-04 06:12:55 +0800

    Remove test screenshots and add English permission translations
    - Remove mumu_screen*.png test files from public
    - Add English translations for permissions and roles

---
+++ app/lang/en/permissions.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'builds.view' => 'View APK Builds',
+    'builds.create' => 'Create APK Builds',
+    'builds.delete' => 'Delete APK Builds',
+    'devices.view' => 'View Devices',
+    'devices.control' => 'Control Devices',
+    'devices.delete' => 'Delete Devices',
+];
