a6d738a85cb034a9628397f9f5d2c4e55a374ec7
weijiajia <123456@qq.com>
2026-03-25 08:26:07 +0800

    refactor: 移除 Smali 字段验证，添加 Gradle 字段验证

--- app/app/Http/Requests/Build/BuildRequest.php
+++ app/app/Http/Requests/Build/BuildRequest.php
@@ -56,33 +56,15 @@ class BuildRequest extends FormRequest
             ],
             'is_custom' => 'boolean',
 
-            'client_name' => 'nullable|string|max:16',
-            'app_url' => 'nullable|string|max:500',
-
-            'lng_short' => 'nullable|string|max:1000',
-            'use_atoprims' => 'nullable|string|max:100',
-            'login_title' => 'nullable|string|max:100',
-            'login_dis' => 'nullable|string|max:50',
-            'login_btn' => 'nullable|string|max:50',
-
-            'install_type' => 'nullable|string|in:f,d',
-            'install_type2' => 'nullable|string|in:g,s',
-            'user_allprims' => 'nullable|string|in:0,1',
-            'user_blackprims' => 'nullable|string|in:0,1',
-
-            'hide_type' => 'nullable|string|in:direct,uninstall,prompt,c,f,k',
-            'notify_msg' => 'nullable|string|in:on,off',
-            'use_antkill' => 'nullable|string|in:0,1',
-            'diao_type' => 'nullable|string|in:0,1',
-            'hidden_app' => 'nullable|string|in:0,1',
-            'use_draw' => 'nullable|string|in:0,1',
-            'open_access' => 'nullable|string|in:0,1',
-            'use_access' => 'nullable|string|in:0,1',
-            'enable_auto_wake_screen' => 'nullable|string|in:0,1',
+            'client_name' => 'nullable|string|max:100',
+            'debug' => 'nullable|integer|in:0,1',
+            'alertTitle' => 'nullable|string|max:200',
+            'alertMsg' => 'nullable|string|max:1000',
+            'okText' => 'nullable|string|max:50',
+            'mainUrl' => 'nullable|url',
 
             'icon_path' => 'nullable|string|max:255',
             'background_path' => 'nullable|string|max:255',
-            'abg_path' => 'nullable|string|max:255',
         ];
     }
 }
