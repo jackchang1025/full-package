61af2cabece315fd6ea610364aded9f71d84841a
weijiajia <123456@qq.com>
2026-04-10 07:36:46 +0800

    feat: add gkd-kit/selector KMP module (4,416 lines, from android project)

---
+++ vendor-replica/selector/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.multiplatform")
+}
+
+kotlin {
+    androidTarget()
+    jvm()
+
+    sourceSets {
+        val commonMain by getting
+        val jvmMain by getting
+        val androidMain by getting {
+            dependsOn(jvmMain)
+        }
+    }
+}
+
+android {
+    namespace = "li.songe.selector"
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 24
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+tasks.withType<Test> {
+    enabled = false
+}
